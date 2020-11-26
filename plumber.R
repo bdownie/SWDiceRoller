8628452fff0c119ff0d5f8c341d990c43dfe1d2b
Bryan Downie <bryand@pobox.com>
2020-11-26 08:42:15 -0800

    ready to test1

--- plumber.R
+++ plumber.R
@@ -21,16 +21,19 @@ rollSWDice <- function(green,purple) {
     # Success, Advantage
     green_die <- list(c(0,0),c(1,0),c(1,0),c(2,0),c(0,1),c(0,1),c(1,1),c(0,2))
     # Failure, Threat
-    purple_die <- list(c(0,0),c(1,0),c(2,0),c(0,1),c(0,1),c(0,1),c(0,2),c(1,1))
+    purple_die <- list(c(0,0),c(-1,0),c(-2,0),c(0,-1),c(0,-1),c(0,-1),c(0,-2),c(-1,-1))
     
     green <- sample(1:8, green, replace=TRUE)
-    green <- psum(sapply(green,function(x) {
+    purple <- sample(1:8, purple, replace=TRUE)
+    
+    green <- colSums(sapply(green,function(x) {
         green_die[[x]]
     }))
-    purple <- psum(sapply(purple,function(x) {
+    purple <- colSums(sapply(purple,function(x) {
         purple_die[[x]]
     }))
+    data.frame(Success=green[1] + purple[1],Advantage=green[2] + purple[2])
     #green2[1]
-    tibble(Success=green[1],Advantage=green[2],Failure=purple[1],Threat=purple[2])
+    #tibble(Success=green[1],Advantage=green[2],Failure=purple[1],Threat=purple[2])
     
 }
